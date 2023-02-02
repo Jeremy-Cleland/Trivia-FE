90d6b2452fcf84ccdf4b1258db479f9404b34359
Jeremy Cleland <jeremy@clelandco.com>
2023-02-02 16:43:30 -0500

    styled formModal

--- src/FormModal.js
+++ src/FormModal.js
@@ -1,88 +1,94 @@
-import React from 'react';
-import Modal from 'react-bootstrap/Modal';
-import { Container, Form, Button } from 'react-bootstrap';
-import categories from './Data/triviaCategories.json';
+import React from "react";
+import Modal from "react-bootstrap/Modal";
+import { Container, Form, Button } from "react-bootstrap";
+import styles from "./FormModal.module.css";
+import categories from "./Data/triviaCategories.json";
 
 class FormModal extends React.Component {
-
   render() {
     return (
-      <>
-        <Modal show={this.props.showModal} onHide={this.props.handleCloseModal}>
-
-          <Modal.Header closeButton>Game Options</Modal.Header>
-          <Modal.Body>
-            <Container className="mt-5">
+      <Container>
+        <Modal
+          className={styles.modal}
+          centered
+          show={this.props.showModal}
+          onHide={this.props.handleCloseModal}
+        >
+          <Modal.Header className={styles.header} closeButton>
+            <Modal.Title>Game Options</Modal.Title>
+          </Modal.Header>
+          <Modal.Body className={styles.body}>
+            <Container className={styles.innerbody}>
               <Form onSubmit={this.props.handleBookSubmit}>
-
-                <Form.Group controlId="Category">
-                  <Form.Label>Category</Form.Label>
+                <Form.Group controlId='Category'>
+                  <Form.Label className={styles.formLabel}>Category</Form.Label>
                   <Form.Select>
-                    <option value="all">Random Categories</option>
-                    {categories.trivia_categories.map(category => (
-                      <option
-                        key={category.id}
-                        value={category.id}>
+                    <option value='all'>Random Categories</option>
+                    {categories.trivia_categories.map((category) => (
+                      <option key={category.id} value={category.id}>
                         {category.name}
                       </option>
                     ))}
                   </Form.Select>
                 </Form.Group>
 
-                <Form.Group controlId="Number of Questions">
-                  <Form.Label>Number of Questions</Form.Label>
-                  <Form.Control
-                    type="number"
-                    min="1"
-                    max="10" />
+                <Form.Group controlId='Number of Questions'>
+                  <Form.Label className={styles.formLabel}>
+                    Number of Questions
+                  </Form.Label>
+                  <Form.Control type='number' min='1' max='10' />
                 </Form.Group>
 
-                <Form.Group controlId="Difficulty Level">
-                  <Form.Label>Difficulty Level</Form.Label>
+                <Form.Group controlId='Difficulty Level'>
+                  <Form.Label className={styles.formLabel}>
+                    Difficulty Level
+                  </Form.Label>
                   <Form.Check
                     inline
-                    name="difficulty"
-                    type="radio"
-                    id="All"
-                    label="All"
+                    name='difficulty'
+                    type='radio'
+                    id='All'
+                    label='All'
                   />
                   <Form.Check
                     inline
-                    name="difficulty"
-                    type="radio"
-                    id="Easy"
-                    label="Easy"
+                    name='difficulty'
+                    type='radio'
+                    id='Easy'
+                    label='Easy'
                   />
                   <Form.Check
                     inline
-                    name="difficulty"
-                    type="radio"
-                    id="Medium"
-                    label="Medium"
+                    name='difficulty'
+                    type='radio'
+                    id='Medium'
+                    label='Medium'
                   />
                   <Form.Check
                     inline
-                    name="difficulty"
-                    type="radio"
-                    id="Hard"
-                    label="Hard"
+                    name='difficulty'
+                    type='radio'
+                    id='Hard'
+                    label='Hard'
                   />
                 </Form.Group>
 
-                <Button type="submit">Submit</Button>
-
+                <Button className={styles.gameButton1} type='submit'>
+                  Submit
+                </Button>
               </Form>
 
               <Button
-                type="button"
-                onClick={this.props.handleFetchQuestions}>
+                className={styles.gameButton2}
+                type='button'
+                onClick={this.props.handleFetchQuestions}
+              >
                 Get New Questions
               </Button>
             </Container>
           </Modal.Body>
-
         </Modal>
-      </>
+      </Container>
     );
   }
 }
